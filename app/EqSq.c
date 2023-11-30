ecd13e0edc70dd98e327a83b4f4224434ec044c2
userRr423 <117460437+userRr423@users.noreply.github.com>
2023-12-01 00:48:31 +0300

    Update EqSq.c

--- app/EqSq.c
+++ app/EqSq.c
@@ -5,42 +5,26 @@
 
 int roots(double a, double b, double c)
 {
-    double D;
+    
+    float discriminant, root1, root2;
 
-    if (a == 0.0 && b == 0.0 && c == 0.0)
+        // Вычисление дискриминанта
+        discriminant = b * b - 4 * a * c;
+
+        // Проверка значения дискриминанта
+        if (discriminant > 0) {
+            // Два различных корня
+            root1 = (-b + sqrt(discriminant)) / (2 * a);
+            root2 = (-b - sqrt(discriminant)) / (2 * a);
+            //printf("Корни уравнения: %.2f и %.2f\n", root1, root2);
+            return root2;
+        } else if (discriminant == 0)
+        {
+            // Один корень
+            root1 = -b / (2 * a);
+            //printf("Корень уравнения: %.2f\n", root1);
+            return root1;
+        }
+
         return -1;
-    else
-    {
-        if (a == 0.0)
-        {
-            if (b == 0.0)
-                return -1;
-
-
-
-            return 1;
-        }
-        else
-        {
-            D = b * b - 4.0 * a * c;
-
-            if (D < 0.0)
-                return 0;
-            else
-            {
-                if (D == 0.0)
-                {
-
-
-                    return 1;
-                }
-                else
-                {
-
-
-                    return 2;
-                }
-            }
-        }
-    }
 }
