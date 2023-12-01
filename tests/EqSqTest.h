e29ca22194d2740f58ea8b22f683fe805e5ecd18
userRr423 <117460437+userRr423@users.noreply.github.com>
2023-12-01 17:37:30 +0300

    Update EqSqTest.h

--- tests/EqSqTest.h
+++ tests/EqSqTest.h
@@ -12,13 +12,44 @@ extern "C" {
 #include "EqSq.h"
 }
 
-
 TEST(MyrootTest, num0) {
-    ASSERT_EQ(roots(1, 2, 3), -1);
+    float root1, root2;
+
+     roots(4, 10, 1, &root1, &root2);
+
+     int g = root1;
+     int g2 = root2;
+
+     ASSERT_EQ(g, 0);
+     ASSERT_EQ(g2, -2);
 }
 
+
+
 TEST(MyrootTest2, num0) {
-    ASSERT_EQ(roots(4, 10, 1), -2);
+    //ASSERT_EQ(roots(4, 10, 1), -2);
+    float root1, root2;
+
+     roots(40, 10, 1, &root1, &root2);
+
+     int g = root1;
+     int g2 = root2;
+
+     ASSERT_EQ(g, 0);
+     ASSERT_EQ(g2, 0);
+}
+
+TEST(MyrootTest3, num0) {
+    //ASSERT_EQ(roots(4, 10, 1), -2);
+    float root1, root2;
+
+     roots(3, 5, 1, &root1, &root2);
+
+     int g = root1;
+     int g2 = root2;
+
+     ASSERT_EQ(g, 0);
+     ASSERT_EQ(g2, -1);
 }
 
 #endif // EQSQTEST_H
