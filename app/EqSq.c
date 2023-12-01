837ec02858d3ddb250d490a92305203a0bd2b452
userRr423 <117460437+userRr423@users.noreply.github.com>
2023-12-01 17:36:11 +0300

    Update EqSq.c

--- app/EqSq.c
+++ app/EqSq.c
@@ -3,28 +3,25 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-int roots(double a, double b, double c)
+void roots(float a, float b, float c, float* root1, float* root2)
 {
-    
-    float discriminant, root1, root2;
+    float discriminant;
 
         // Вычисление дискриминанта
         discriminant = b * b - 4 * a * c;
 
         // Проверка значения дискриминанта
-        if (discriminant > 0) {
+        if (discriminant >= 0) {
             // Два различных корня
-            root1 = (-b + sqrt(discriminant)) / (2 * a);
-            root2 = (-b - sqrt(discriminant)) / (2 * a);
-            //printf("Корни уравнения: %.2f и %.2f\n", root1, root2);
-            return root2;
-        } else if (discriminant == 0)
-        {
+            *root1 = (-b + sqrt(discriminant)) / (2 * a);
+            *root2 = (-b - sqrt(discriminant)) / (2 * a);
+        } else if (discriminant == 0) {
             // Один корень
-            root1 = -b / (2 * a);
-            //printf("Корень уравнения: %.2f\n", root1);
-            return root1;
+            *root1 = -b / (2 * a);
+            *root2 = *root1;
+        } else {
+            // Комплексные корни
+            *root1 = (-b) / (2 * a);
+            *root2 = sqrt(-discriminant) / (2 * a);
         }
-
-        return -1;
 }
